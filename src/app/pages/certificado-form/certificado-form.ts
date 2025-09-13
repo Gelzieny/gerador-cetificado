78f7e1bd8b3aca5b4cfb095f35e585c704d728e5
Gelzieny R. Martins <gelzieny@gmail.com>
2025-09-13 20:04:06 -0300

    Formulario

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
-import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { CommonModule } from '@angular/common';
+import { FormsModule, type NgModel } from '@angular/forms';
+
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import type { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule ],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
 export class CertificadoForm {
+  certificado: Certificado = {
+    atividades: [],
+    nome: '',
+  };
+  atividade: string = '';
 
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
+
+  formValido() {
+    return (
+      this.certificado.atividades.length > 0 && this.certificado.nome.length > 0
+    );
+  }
+
+  adicionarAtividade() {
+    this.certificado.atividades.push(this.atividade);
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number) {
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit() {
+    if (!this.formValido()) {
+      return;
+    }
+    console.log(this.certificado);
+  }
 }
